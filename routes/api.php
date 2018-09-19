2e0d4b589ce3f70c6a624a188c34475fe5223fc0
EdisonK <zhd@thinkerx.com>
2018-09-19 10:27:17 +0800

    啦啦啦

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['middleware' => ['auth:api']], function () {
+Route::group(['middleware' => ['auth']], function () {
     //后面要加权限必须登陆的才行
     Route::post('orders','OrdersController@store');
 
