1487bb9e86b49729448ce3235cb5097cf93a6b2f
EdisonK <zhd@thinkerx.com>
2018-11-04 12:03:31 +0800

    编辑页面的排版

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -2,92 +2,174 @@
 @section('content')
     <div class="col-lg-9 col-md-9 col-sm-9 pull-left">
 
-
-
-
-        {{--<div class="form-group">--}}
-            {{--<div class="row">--}}
-                {{--<div class="col-lg-6">--}}
-                    {{--<div class="input-group ">--}}
-                         {{--<label for="name1">名称:</label>--}}
-                        {{--<input type="text" class="form-control" id="name1" placeholder="Search for...">--}}
-                    {{--</div><!-- /input-group -->--}}
-                {{--</div><!-- /.col-lg-6 -->--}}
-                {{--<div class="col-lg-6">--}}
-                    {{--<div class="input-group">--}}
-                        {{--<input type="text" class="form-control" placeholder="Search for...">--}}
-                    {{--</div><!-- /input-group -->--}}
-                {{--</div><!-- /.col-lg-6 -->--}}
-            {{--</div><!-- /.row -->--}}
-        {{--</div>--}}
-
-        <div class="row">
-            <div class="col-md-4  input-group-sm">
-                <label for="name" class="col-sm-4">名称:</label> <input class="col-sm-8 form-control" id="name" value="{{ $product->name }}">
+        <form class="form-horizontal" role="form">
+            <div class="form-group  col-md-6">
+                <label class="col-md-4 control-label" for="name">名称：</label>
+                <div class="col-md-8">
+                    <input type="text" class="form-control" id="name" value="{{ $product->name }}">
+                </div>
             </div>
-            <div class="col-md-4 form-inline input-group-sm">
-                <label for="name" >名称:</label> <input class="form-control" id="name" value="{{ $product->name }}">
-            </div>
-            <div class="col-md-4 form-inline input-group-sm">
-                <label for="name" >名称:</label> <input class="form-control" id="name" value="{{ $product->name }}">
-            </div>
-            <div class="col-md-4 form-inline input-group-sm">
-                <label for="name" >名称:</label> <input class="form-control" id="name" value="{{ $product->name }}">
-            </div>
-            <div class="col-md-4 form-inline input-group-sm">
-                <label for="name" >名称:</label> <input class="form-control" id="name" value="{{ $product->name }}">
-            </div>
-            <div class="col-md-4 form-inline input-group-sm">
-                <label for="name" >名称:</label> <input class="form-control" id="name" value="{{ $product->name }}">
-            </div><div class="col-md-4 form-inline input-group-sm">
-                <label for="name" >名称:</label> <input class="form-control" id="name" value="{{ $product->name }}">
-            </div>
-            <div class="col-md-4 form-inline input-group-sm">
-                <label for="name" >名称:</label> <input class="form-control" id="name" value="{{ $product->name }}">
-            </div>
-            <div class="col-md-4 form-inline input-group-sm">
-                <label for="name" >名称:</label> <input class="form-control" id="name" value="{{ $product->name }}">
-            </div>
-            <div class="col-md-4 form-inline input-group-sm">
-                <label for="name" >名称:</label> <input class="form-control" id="name" value="{{ $product->name }}">
+            <div class="form-group col-md-6">
+                <label class="col-md-4 control-label" for="chinese_name">中文名：</label>
+                <div class="col-md-8">
+                    <input type="text" class="form-control" id="chinese_name" value="{{ $product->chinese_name }}">
+                </div>
             </div>
 
 
+            <div class="form-group  col-md-6">
+                <label class="col-md-4 control-label" for="english_name">英文名：</label>
+                <div class="col-md-8">
+                    <input type="text" class="form-control" id="english_name" value="{{ $product->english_name }}">
+                </div>
+            </div>
+            <div class="form-group col-md-6">
+                <label class="col-md-4 control-label" for="cas">CAS号：</label>
+                <div class="col-md-8">
+                    <input type="text" class="form-control" id="cas" value="{{ $product->cas }}">
+                </div>
+            </div>
+
+            <div class="form-group  col-md-6">
+                <label class="col-md-4 control-label" for="molecular_formula">分子式：</label>
+                <div class="col-md-8">
+                    <input type="text" class="form-control" id="molecular_formula" value="{{ $product->molecular_formula }}">
+                </div>
+            </div>
+            <div class="form-group col-md-6">
+                <label class="col-md-4 control-label" for="brand_name">品牌：</label>
+                <div class="col-md-8">
+                    <input type="text" class="form-control" id="brand_name" value="{{ $product->brand->name }}">
+                </div>
+            </div>
+
+            <div class="form-group  col-md-6">
+                <label class="col-md-4 control-label" for="price">单价：</label>
+                <div class="col-md-8">
+                    <input type="text" class="form-control" id="price" value="{{ $product->price }}">
+                </div>
+            </div>
+            <div class="form-group col-md-6">
+                <label class="col-md-4 control-label" for="unit">单位：</label>
+                <div class="col-md-8">
+                    <input type="text" class="form-control" id="unit" value="{{ $product->unit }}">
+                </div>
+            </div>
+
+            <div class="form-group  col-md-6">
+                <label class="col-md-4 control-label" for="model_type">型号：</label>
+                <div class="col-md-8">
+                    <input type="text" class="form-control" id="model_type" value="{{ $product->model_type }}">
+                </div>
+            </div>
+            <div class="form-group col-md-6">
+                <label class="col-md-4 control-label" for="category_name">种类：</label>
+                <div class="col-md-8">
+                    <input type="text" class="form-control" id="category_name" value="{{ $product->category->name }}">
+                </div>
+            </div>
+
+            <div class="form-group  col-md-6">
+                <label class="col-md-4 control-label" for="model_type">型号：</label>
+                <div class="col-md-8">
+                    <input type="text" class="form-control" id="model_type" value="{{ $product->model_type }}">
+                </div>
+            </div>
+            <div class="form-group col-md-12" style="margin-left: -20px;">
+                <label class="col-md-2 control-label" for="company-content">简介：</label>
+                <div class="col-md-10">
+                    <textarea placeholder="Enter description"
+                              style="resize: vertical"
+                              id="company-content"
+                              name="description"
+                              rows="5" spellcheck="false"
+                              class="form-control  text-left">
+                           </textarea>
+                </div>
+            </div>
 
 
 
 
 
+        </form>
 
+        {{--<form class="form-horizontal" role="form">--}}
+            {{--<div class="form-group col-md-4">--}}
+                {{--<label for="firstname" class="col-sm-4 control-label">名字</label>--}}
+                {{--<div class="col-sm-8">--}}
+                    {{--<input type="text" class="form-control" id="firstname" placeholder="请输入名字">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            {{--<div class="form-group col-md-4">--}}
+                {{--<label for="firstname" class="col-sm-4 control-label">名字</label>--}}
+                {{--<div class="col-sm-8">--}}
+                    {{--<input type="text" class="form-control" id="firstname" placeholder="请输入名字">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            {{--<div class="form-group col-md-4">--}}
+                {{--<label for="firstname" class="col-sm-4 control-label">名字</label>--}}
+                {{--<div class="col-sm-8">--}}
+                    {{--<input type="text" class="form-control" id="firstname" placeholder="请输入名字">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            {{--<div class="form-group col-md-4">--}}
+                {{--<label for="firstname" class="col-sm-4 control-label">名字</label>--}}
+                {{--<div class="col-sm-8">--}}
+                    {{--<input type="text" class="form-control" id="firstname" placeholder="请输入名字">--}}
+                {{--</div>--}}
+            {{--</div>--}}
 
-            <div class="col-md-4">
-                <label>中文名称:</label> <span>{{ $product->chinese_name }}</span>
-            </div>
-            <div class="col-md-4">
-                <label>英文名称:</label> <span>{{ $product->english_name }}</span>
-            </div>
-            <div class="col-md-4">
-                <label>CAS 号:</label> <span>{{ $product->cas }}</span>
-            </div>
-            <div class="col-md-4">
-                <label>分子式:</label> <span>{{ $product->molecular_formula }}</span>
-            </div>
-            <div class="col-md-4">
-                <label>品牌:</label> <span>{{ $product->brand->name }}</span>
-            </div>
-            <div class="col-md-4">
-                <label>单价:</label> <span>{{ $product->price }}/{{ $product->unit }}</span>
-            </div>
-            <div class="col-md-4">
-                <label>型号:</label> <span>{{ $product->model_type }}</span>
-            </div>
-            <div class="col-md-4">
-                <label>所属种类:</label> <span>{{ $warehouse->name }}/{{ $class->name }}/{{ $category->name }}</span>
-            </div>
-            <div class="col-md-4">
-                <label>简介:</label> <span>{{ $warehouse->description }}</span>
-            </div>
-        </div>
+            {{--<div class="form-group col-md-4">--}}
+                {{--<label for="firstname" class="col-sm-4 control-label">名字</label>--}}
+                {{--<div class="col-sm-8">--}}
+                    {{--<input type="text" class="form-control" id="firstname" placeholder="请输入名字">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            {{--<div class="form-group col-md-4">--}}
+                {{--<label for="firstname" class="col-sm-4 control-label">名字</label>--}}
+                {{--<div class="col-sm-8">--}}
+                    {{--<input type="text" class="form-control" id="firstname" placeholder="请输入名字">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            {{--<div class="form-group col-md-4">--}}
+                {{--<label for="firstname" class="col-sm-4 control-label">名字</label>--}}
+                {{--<div class="col-sm-8">--}}
+                    {{--<input type="text" class="form-control" id="firstname" placeholder="请输入名字">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            {{--<div class="form-group col-md-4">--}}
+                {{--<label for="firstname" class="col-sm-4 control-label">名字</label>--}}
+                {{--<div class="col-sm-8">--}}
+                    {{--<input type="text" class="form-control" id="firstname" placeholder="请输入名字">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            {{--<div class="form-group col-md-4">--}}
+                {{--<label for="firstname" class="col-sm-4 control-label">名字</label>--}}
+                {{--<div class="col-sm-8">--}}
+                    {{--<input type="text" class="form-control" id="firstname" placeholder="请输入名字">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            {{--<div class="form-group col-md-4">--}}
+                {{--<label for="firstname" class="col-sm-4 control-label">名字</label>--}}
+                {{--<div class="col-sm-8">--}}
+                    {{--<input type="text" class="form-control" id="firstname" placeholder="请输入名字">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            {{--<div class="form-group col-md-4">--}}
+                {{--<label for="firstname" class="col-sm-4 control-label">名字</label>--}}
+                {{--<div class="col-sm-8">--}}
+                    {{--<input type="text" class="form-control" id="firstname" placeholder="请输入名字">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            {{--<div class="form-group col-md-4">--}}
+                {{--<label for="firstname" class="col-sm-4 control-label">名字</label>--}}
+                {{--<div class="col-sm-8">--}}
+                    {{--<input type="text" class="form-control" id="firstname" placeholder="请输入名字">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+
+        {{--</form>--}}
     </div>
     <div class="col-sm-3 col-md-3 col-lg-3 pull-right">
         <div class="sidebar-module">
