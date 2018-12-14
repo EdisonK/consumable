48cca510f351e23144249555c576e615246529d5
EdisonK <zhd@thinkerx.com>
2018-12-14 19:07:51 +0800

    修改bug

--- resources/views/admin/products/show.blade.php
+++ resources/views/admin/products/show.blade.php
@@ -33,6 +33,9 @@
             <div class="col-md-4">
                 <label>所属种类:</label> <span>{{ $warehouse->name }}/{{ $class->name }}/{{ $category->name }}</span>
             </div>
+
+        </div>
+        <div class="row">
             <div class="col-md-4">
                 <label>简介:</label> <span>{{ $product->description }}</span>
             </div>
